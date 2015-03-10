65088f3c4270ffab8bcd2f8b2475da7c13980789
jessica <ladyjtm@gmail.com>
2015-03-10 10:46:09 -0700

    Add initial files. No functionality

---
+++ js/app.js
@@ -0,0 +1,30 @@
+var taskInput;  //new task
+
+var addButton; //firstbutton
+
+var incompleteTasksHolder;
+
+var completedTasksHolder;
+
+
+var addTask = function() {
+
+}
+
+var deleteTask = function() {
+
+}
+
+
+var editTask = function(){
+
+}
+
+var taskComplete = function() {
+
+}
+
+
+var taskIncomplete = function() {
+
+}
